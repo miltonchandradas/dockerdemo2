0cba55afbb6ef5b09b0698766903d486a4c4f410
Milton Chandradas <milton.chandradas@sap.com>
2020-10-16 10:18:31 -0400

    second commit...

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ const handleProductsRequest = async (req, res) => {
    res.send(response.data.d.results);
 };
 
+const handleSuppliersRequest = async (req, res) => {
+   const response = await axios.get(
+      "https://services.odata.org/V2/Northwind/Northwind.svc/Suppliers?$format=json"
+   );
+   res.send(response.data.d.results);
+};
+
 app.get("/products", handleProductsRequest);
+app.get("/suppliers", handleSuppliersRequest);
 
 app.listen(PORT, () => {
    console.log(`Listening on port ${PORT}`);
